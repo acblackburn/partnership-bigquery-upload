ff2af552c92238010d422b218ce8fcb447baf3a9
acblackburn <alexcblackburn98@gmail.com>
2020-06-15 17:41:14 +0100

    Modified gbq_transfer.py to mimic append_test.py
    - cleaned up "object" to "STRING" BigQuery schema specification.

--- gbq_transfer.py
+++ gbq_transfer.py
@@ -2,19 +2,16 @@ from google.cloud import bigquery
 import pandas as pd
 
 def df_to_gbq(df, table_name):
-
+    
     client = bigquery.Client()
     table_id = "example-data-pipeline.gbq_test." + table_name
     schema = []
 
     # Identify dataframe columns of a object (string) datatype
     df_dtypes = df.dtypes
-    df_strings = df_dtypes[df_dtypes == 'object']
-    df_strings = df_strings.index.tolist()
-
-    # Specify bigquery "STRING" datatypes
-    for column in df_strings:
-        schema.append(bigquery.SchemaField(column, "STRING"))
+    for column, dtype in df_dtypes.items():
+        if dtype == "object":
+            schema.append(bigquery.SchemaField(column, "STRING"))
     job_config = bigquery.LoadJobConfig(schema=schema)
 
     # Load DataFrame in BigQuery

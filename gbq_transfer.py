a1a27396b718f57e3312609d62586a4d6ea41691
acblackburn <alexcblackburn98@gmail.com>
2020-06-16 13:55:22 +0100

    Starting to implement filename/path for google cloud function

--- gbq_transfer.py
+++ gbq_transfer.py
@@ -1,9 +1,14 @@
-from google.cloud import bigquery
+from google.cloud import bigquery, storage
 import pandas as pd
+import os
+
+def gcs_to_df(bucket_name):
+    storage_client = storage.Client()
+
+    blobs = storage_client.list_blobs(bucket_name)
 
 def df_to_gbq(df, table_name):
-    
-    client = bigquery.Client()
+    bq_client = bigquery.Client()
     table_id = "example-data-pipeline.gbq_test." + table_name
     schema = []
 
@@ -15,15 +20,13 @@ def df_to_gbq(df, table_name):
     job_config = bigquery.LoadJobConfig(schema=schema)
 
     # Load DataFrame in BigQuery
-    job = client.load_table_from_dataframe(
+    job = bq_client.load_table_from_dataframe(
         df, table_id, job_config=job_config
     )
 
     # Wait for load job to complete
     job.result()
 
-
-
-
-
-
+def event_file_info(string):
+    path, filename = os.path.split(string)
+    return (path, filename)

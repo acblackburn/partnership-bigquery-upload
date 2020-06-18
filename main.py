b585a59095f5106dd548e652cdd0795c22baa6fd
acblackburn <alexcblackburn98@gmail.com>
2020-06-18 17:55:48 +0100

    Modified main for cloud function

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import file_upload_clean as fc
 from gbq_transfer import df_to_gbq, event_file_info
-'''
+
 def main(event, context):
     file_path, file_name = event_file_info(event['name'])
     
     if file_path == "Budget":
-        budget_df = fc.clean_budgetsheet(event)
-        df_to_gbq(budget_df, "Budget")
+        budget_df = fc.clean_budget(event)
+        df_to_gbq(budget_df, "Finance", "Budget")
     elif file_path == "eConsult":
         pass
 
     # Delete original uploaded file
-'''
-def main():
+
+""" def main():
     budget_df = fc.clean_budget("Budgetv1.csv")
     print(budget_df.head())
     df_to_gbq(budget_df, "Finance", "Budget")
 
 if __name__ == "__main__":
-    main()
+    main() """

6a33b6b5834af5551d858e4ed233956959f2386f
acblackburn <alexcblackburn98@gmail.com>
2020-06-21 17:23:00 +0100

    testing event/context cloud function

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import localpackage.file_upload_clean as fc
 from localpackage.gbq_transfer import df_to_gbq, event_file_info
 
-""" def main(event, context):
+def main(event, context):
     file_path, file_name = event_file_info(event['name'])
-    
-    if file_path == "Budget":
+    print(file_name, file_path)
+    """ if file_path == "Budget":
         budget_df = fc.clean_budget(event)
         df_to_gbq(budget_df, "Finance", "Budget")
     elif file_path == "eConsult":
         pass """
 
-def main():
+""" def main():
     budget_df = fc.clean_budget("MODGRP - May 20 TB data.csv")
     df_to_gbq(budget_df, "Finance", "Budget")
 
 if __name__ == "__main__":
-    main()
+    main() """

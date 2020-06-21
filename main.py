3028448d96b598db5352f22a74f3eac21aee76ea
acblackburn <alexcblackburn98@gmail.com>
2020-06-21 18:16:33 +0100

    Test cloud function using full bucket name

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import localpackage.file_upload_clean as fc
 from localpackage.gbq_transfer import df_to_gbq, event_file_info
 
-def main(event, context):
-    file_path, file_name = event_file_info(event['name'])
+def main(data, context):
+    file_path, file_name = event_file_info(data['name'])
 
     if file_path == "Budget":
-        budget_df = fc.clean_budget(event['name'])
+        budget_df = fc.clean_budget(f"gs://{data['bucket']}/{data['name']}")
         df_to_gbq(budget_df, "Finance", "Budget_test")
     elif file_path == "eConsult":
         pass

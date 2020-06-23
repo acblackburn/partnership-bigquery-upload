4bc46782b79ac62633bcf3203f25974ba3be3b44
acblackburn <alexcblackburn98@gmail.com>
2020-06-23 14:10:48 +0100

    Added bigquery schema specifiction using metadata json file

--- localpackage/gbq_transfer.py
+++ localpackage/gbq_transfer.py
@@ -12,38 +12,8 @@ def df_to_gbq(df, dataset, table_name):
     data = json.load(json_file)
     budget_metadata = data['budget']
     
-    schema = []
-    for entry in budget_metadata:
-        list_append = bigquery.ShemaField(entry['bq_name'],entry['bq_dtype'])
-        schema.append(list_append)
-    
-    # TODO redo this section using json file
-    schema = [
-        bigquery.SchemaField("Year", "STRING"),
-        bigquery.SchemaField("Month", "STRING"),
-        bigquery.SchemaField("MonthNumeric", "STRING"),
-        bigquery.SchemaField("Date", "DATE"),
-        bigquery.SchemaField("Account", "STRING"),
-        bigquery.SchemaField("A_C_Ref", "STRING"),
-        bigquery.SchemaField("CAT", "STRING"),
-        bigquery.SchemaField("Reporting_Code", "STRING"),
-        bigquery.SchemaField("Reporting_Description", "STRING"),
-        bigquery.SchemaField("CC", "STRING"),
-        bigquery.SchemaField("Dp", "STRING"),
-        bigquery.SchemaField("YTD", "FLOAT"),
-        bigquery.SchemaField("Income_Expenses", "STRING"),
-        bigquery.SchemaField("List_Size", "INTEGER"),
-        bigquery.SchemaField("Period_1000", "FLOAT"),
-        bigquery.SchemaField("YTD_1000", "FLOAT"),
-        bigquery.SchemaField("Practice_Weighted_List_Size", "FLOAT"),
-        bigquery.SchemaField("Practice_Raw_List_Size", "FLOAT"),
-        bigquery.SchemaField("Divisional_Weighted_List_Size", "FLOAT"),
-        bigquery.SchemaField("Divisional_raw_List_Size", "FLOAT"),
-        bigquery.SchemaField("YTD_practice_weighted1000", "FLOAT"),
-        bigquery.SchemaField("YTD_practice_raw_1000", "FLOAT"),
-        bigquery.SchemaField("YTD_Divisional_weighted_1000", "FLOAT"),
-        bigquery.SchemaField("YTD_Divisional_raw_1000", "FLOAT")
-    ]
+    # Create bq schema from json metadata 
+    schema = [bigquery.SchemaField(entry['bq_name'], entry['bq_dtype']) for entry in budget]
 
     job_config = bigquery.LoadJobConfig(schema=schema)
 
@@ -52,8 +22,9 @@ def df_to_gbq(df, dataset, table_name):
         df, table_id, job_config=job_config
     )
 
-    # Wait for load job to complete
+    # Wait for load job to complete anc close open json file
     job.result()
+    json_file.close()
 
 def event_file_info(string):
     path, filename = os.path.split(string)

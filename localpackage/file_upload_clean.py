4534b0f6dd92f1534ff75cd22cd5e4721d689577
ruscott <ru.scott24@gmail.com>
2020-06-25 16:44:17 +0100

    Fixed highlighted problem in eConsult function.

--- localpackage/file_upload_clean.py
+++ localpackage/file_upload_clean.py
@@ -68,83 +68,46 @@ def age_bracket(int):
 
 def consultations_clean(input_file):
 
+    # Open and load json metadata file
+    json_file = open("metadata.json")
+    data = json.load(json_file)
+    practice_lut = data['practice_lut']
+
+    #read in data
     usage_df = pd.read_excel('econsult.xlsx', sheet_name='Usage', skiprows=21)
     reason_df = pd.read_excel('econsult.xlsx', sheet_name='All Consults')
     e_consult_nhse = pd.read_excel('econsult.xlsx', sheet_name='NHSE')
 
-    #MAKE JSON FILE
-    practice_lookuptable = {'M88013':{'div':'BIR','practice_code':'BCF'}, 
-                        'M85124':{'div':'BIR','practice_code':'BEL'}, 
-                        'G85124':{'div':'LEW','practice_code':'BGS'}, 
-                        'H81037':{'div':'SUR','practice_code':'BMP'}, 
-                        'K81638':{'div':'WOK','practice_code':'BUR'}, 
-                        'H82040':{'div':'SUS','practice_code':'CHD'}, 
-                        'B82020':{'div':'AWC','practice_code':'CRS'}, 
-                        'M85178':{'div':'BIR','practice_code':'ENK'},
-                        'B83021':{'div':'AWC','practice_code':'FAR'},
-                        'B82028':{'div':'AWC','practice_code':'FIS'},
-                        'M85002':{'div':'BIR','practice_code':'HWM'},
-                        'B83027':{'div':'AWC','practice_code':'HAW'},
-                        'M85016':{'div':'BIR','practice_code':'HIL'},
-                        'B83023':{'div':'AWC','practice_code':'HOL'},
-                        'G85004':{'div':'LEW','practice_code':'JEN'},
-                        'H82010':{'div':'SUS','practice_code':'JCS'},
-                        'B83033':{'div':'AWC','practice_code':'KIL'},
-                        'M85069':{'div':'BIR','practice_code':'LPH'},
-                        'M91028':{'div':'WAL','practice_code':'BER'},
-                        'M91032':{'div':'WAL','practice_code':'COL'},
-                        'M91660':{'div':'WAL','practice_code':'VAI'},
-                        'M91613':{'div':'WAL','practice_code':'FOR'},
-                        'Y02627':{'div':'WAL','practice_code':'HAR'},
-                        'B81048':{'div':'HUL','practice_code':'NEL'},
-                        'M91016':{'div':'WAL','practice_code':'NIN'},
-                        'B83061':{'div':'AWC','practice_code':'OAK'},
-                        'H82621':{'div':'SUS','practice_code':'PVH'},
-                        'M91616':{'div':'WAL','practice_code':'PLE'},
-                        'H82008':{'div':'SUS','practice_code':'SHI'},
-                        'B83006':{'div':'AWC','practice_code':'SIL'}, 
-                        'H81638':{'div':'SUR','practice_code':'SMA'},
-                        'M88002':{'div':'BIR','practice_code':'SMC'},
-                        'G85005':{'div':'LEW','practice_code':'SLG'},
-                        'M91007':{'div':'WAL','practice_code':'SJO'},
-                        'Y02626':{'div':'WAL','practice_code':'KEY'},
-                        'H81060':{'div':'SUR','practice_code':'TMP'},
-                        'K81022':{'div':'WOK','practice_code':'WOK'}}
+    #look up table for list size created from NHSE df
+    list_size_lookuptable = {row[0]:row[3] for index,row in e_consult_nhse.iterrows()}
 
-    ods_code = usage_df['ODS Code'].unique()
-    list_size_lookuptable = {}
-    for index,row in e_consult_nhse.iterrows():
-        for code in ods_code:
-            if code == row[0]:
-                if code not in list_size_lookuptable:
-                    list_size_lookuptable[code] = row[3]
-
-    #dictionary of divisional list lisze from suming practice list sizes
+    #dictionary of divisional list size from suming practice list sizes
     #obtaining individual divisions
-    div = set( val['div'] for val in practice_lookuptable.values())
+    div = set(entry['DIV'] for entry in practice_lut)
 
+    #need to make this better
     #division look up table created
     division_list_size_lut = {}
     for division in div:
-        for key in practice_lookuptable:
-            if practice_lookuptable[key]['div'] == division:
+        for entry in practice_lut:
+            if entry['DIV'] == division: 
                 if division not in division_list_size_lut:
-                    division_list_size_lut[division] = list_size_lookuptable[key]
+                    division_list_size_lut[division] = list_size_lookuptable[entry['ODS Code']]
                 else: 
-                    division_list_size_lut[division] += list_size_lookuptable[key]
+                    division_list_size_lut[division] += list_size_lookuptable[entry['ODS Code']]
     
     #Data for reaon dataframe
     #To add divison to reson dataframe
-    reason_df['DIV'] = reason_df['ODS Code'].apply(lambda x: practice_lookuptable[x]['div'])
-    
-    #To add practice code to reson dataframe
-    reason_df['Code'] = reason_df['ODS Code'].apply(lambda x: practice_lookuptable[x]['practice_code'])
+    reason_df['DIV'] = reason_df['ODS Code'].map({entry['ODS Code']:entry['DIV'] for entry in practice_lut})
+   
+   #To add practice code to reson dataframe
+    reason_df['Code'] = reason_df['ODS Code'].map({entry['ODS Code']:entry['practice_code'] for entry in practice_lut})
 
     #To add list size to reason dataframe
     reason_df['List Size'] = reason_df['ODS Code'].apply(lambda x: list_size_lookuptable[x])
     
     #To add divisional list size to reason dataframe
-    reason_df['Div List'] = reason_df['ODS Code'].apply(lambda x: division_list_size_lut[practice_lookuptable[x]['div']] )
+    reason_df['Div List'] = reason_df['DIV'].apply(lambda x: division_list_size_lut[x])
 
     #To add age bracket to reason datafram
     reason_df['Age Bracket'] = reason_df['Age'].apply(age_bracket)
@@ -169,6 +132,7 @@ def consultations_clean(input_file):
     day = "01"
     month = reason_df['Month'][0]
     year = reason_df['Date'][0].strftime("%Y")
+    "{}/{}/{}".format(day,month,year)
     date = day + "/" + month + "/" + year
     usage_df['Month'] = datetime.strptime(date,"%d/%B/%Y")
 
@@ -177,4 +141,8 @@ def consultations_clean(input_file):
 
     reason_df = reason_df.drop('ODS Code', axis=1)
     usage_df = usage_df.drop(['ODS Code','Practice Id','Practice Type'], axis=1)
+
+    #close json_file
+    json_file.close()
+
     return usage_df, reason_df

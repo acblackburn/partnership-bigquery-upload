1bbcd1964dd91300c7eaff9490faa37e5132252f
acblackburn <alexcblackburn98@gmail.com>
2020-07-06 18:59:15 +0100

    Chnaged varaible names in an attempt to fix cloud function

--- main.py
+++ main.py
@@ -5,22 +5,19 @@ def main(data, context):
     file_path, file_name = data_file_info(data['name'])
     
     if file_path == "Budget":
-        budget_df = fc.clean_budget(f"gs://{data['bucket']}/{data['name']}")
+        df_budget = fc.clean_budget(f"gs://{data['bucket']}/{data['name']}")
         df_to_gbq(budget_df, "Finance", "Budget")
     elif file_path == "eConsult":
-        usage_df, reason_df = fc.clean_consultations(f"gs://{data['bucket']}/{data['name']}")
-        print(usage_df.head())
-        # df_to_gbq(usage_df, "eConsult", "Usage")
-        # df_to_gbq(reason_df, "eConsult", "Reason")
+        df_usage, df_reason = fc.clean_consultations(f"gs://{data['bucket']}/{data['name']}")
+        df_to_gbq(df_usage, "eConsult", "Usage")
+        df_to_gbq(df_reason, "eConsult", "Reason")
 
     delete_blob(data['bucket'], data['name'])
 
 # def main():
-#     test_df_usage, test_df_reason = fc.clean_consultations("eConsult_usage_report_for_Modality__2020060120200630.xlsx")
+#     test_df_usage, test_df_reason = fc.clean_consultations("gs://dashboards-data-upload/eConsult/eConsult_usage_report_for_Modality__2020060120200630.xlsx")
 #     df_to_gbq(test_df_usage, "eConsult", "Usage")
 #     df_to_gbq(test_df_reason, "eConsult", "Reason")
-#     # test_df = fc.clean_budget("Budgetv1.xlsx")
-#     # df_to_gbq(test_df, "Finance", "Budget")
 
 # if __name__ == "__main__":
 #     main()

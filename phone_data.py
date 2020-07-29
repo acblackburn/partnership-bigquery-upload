a6ddf18e388b400824260641bfb05d53287fc102
acblackburn <alexcblackburn98@gmail.com>
2020-07-29 12:00:50 +0100

    Replaced white space in column names with underscores for phone_data.py

--- phone_data.py
+++ phone_data.py
@@ -59,22 +59,25 @@ for df in df_list:
 
 full_df.reset_index(drop=True, inplace=True)
 
+full_df.columns = full_df.columns.str.replace(' ', '_')
+
+print(full_df.dtypes)
 # full_df.to_csv("~/Desktop/phone_data_clean.csv", index=False)
 
-bq_client = bigquery.Client(project="modalitydashboards")
-table_id = f"modalitydashboards.phone_data.example_month"
+# bq_client = bigquery.Client(project="modalitydashboards")
+# table_id = f"modalitydashboards.phone_data.example_month"
 
-schema = [
-    bigquery.SchemaField(),
-    ...
-]
+# schema = [
+#     bigquery.SchemaField(),
+#     ...
+# ]
 
-job_config = bigquery.LoadJobConfig(schema=schema)
+# job_config = bigquery.LoadJobConfig(schema=schema)
 
-# Load DataFrame in BigQuery
-job = bq_client.load_table_from_dataframe(
-    full_df, table_id, job_config=job_config
-)
+# # Load DataFrame in BigQuery
+# job = bq_client.load_table_from_dataframe(
+#     full_df, table_id, job_config=job_config
+# )
 
-# Wait for load job to complete
-job.result()
+# # Wait for load job to complete
+# job.result()

fb8dc335816fcd9c917bff82aabd5c96a55631f4
acblackburn <alexcblackburn98@gmail.com>
2020-08-01 12:30:35 +0100

    Updated phone_data.py

--- phone_data.py
+++ phone_data.py
@@ -72,30 +72,30 @@ full_df.reset_index(drop=True, inplace=True)
 
 full_df.columns = full_df.columns.str.replace(' ', '_')
 
-full_df.to_csv("~/Desktop/phone_data_clean.csv", index=False)
+# full_df.to_csv("~/Desktop/phone_data_clean.csv", index=False)
 
-# bq_client = bigquery.Client(project="modalitydashboards")
-# table_id = f"modalitydashboards.phone_data.example_month"
+bq_client = bigquery.Client(project="modalitydashboards")
+table_id = f"modalitydashboards.phone_data.example_month"
 
-# schema = [
-#     bigquery.SchemaField("Start_time", "TIMESTAMP"),
-#     bigquery.SchemaField("Duration", "STRING"),
-#     bigquery.SchemaField("Event_type", "STRING"),
-#     bigquery.SchemaField("Device_type", "STRING"),
-#     bigquery.SchemaField("Reporting", "STRING"),
-#     bigquery.SchemaField("Full_name", "STRING"),
-#     bigquery.SchemaField("Comment", "STRING"),
-#     bigquery.SchemaField("Call_direction", "STRING"),
-#     bigquery.SchemaField("Location", "STRING"),
-#     bigquery.SchemaField("Call_ID", "STRING")
-# ]
+schema = [
+    bigquery.SchemaField("Start_time", "TIMESTAMP"),
+    bigquery.SchemaField("Duration", "STRING"),
+    bigquery.SchemaField("Event_type", "STRING"),
+    bigquery.SchemaField("Device_type", "STRING"),
+    bigquery.SchemaField("Reporting", "STRING"),
+    bigquery.SchemaField("Full_name", "STRING"),
+    bigquery.SchemaField("Comment", "STRING"),
+    bigquery.SchemaField("Call_direction", "STRING"),
+    bigquery.SchemaField("Location", "STRING"),
+    bigquery.SchemaField("Call_ID", "STRING")
+]
 
-# job_config = bigquery.LoadJobConfig(schema=schema)
+job_config = bigquery.LoadJobConfig(schema=schema)
 
-# # Load DataFrame in BigQuery
-# job = bq_client.load_table_from_dataframe(
-#     full_df, table_id, job_config=job_config
-# )
+# Load DataFrame in BigQuery
+job = bq_client.load_table_from_dataframe(
+    full_df, table_id, job_config=job_config
+)
 
-# # Wait for load job to complete
-# job.result()
+# Wait for load job to complete
+job.result()

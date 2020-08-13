a96ea232a42f32d289af771bf3bd34f0930dfa77
acblackburn <alexcblackburn98@gmail.com>
2020-08-13 14:13:48 +0100

    Creating empty Period columns in initial budget dataset

--- initial_budget_upload.py
+++ initial_budget_upload.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import xlrd
+import json
 from datetime import datetime
 
 # Open and load json metadata file
@@ -9,5 +10,11 @@ with open("metadata.json") as json_file:
     budget_metadata = data['Budget']
 
 df = pd.read_csv("~/Desktop/Budgetv1.csv")
+
+df['Period'] = None
+df['Period_Practice_Weighted_1000'] = None
+df['Period_Practice_Raw_1000'] = None
+df['Period_Divisional_Weighted_1000'] = None
+df['Period_Divisional_Raw_1000'] = None
 
 print(df.dtypes)

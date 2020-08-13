fe8cbd4d032203df4a83f0a37b90bb22ae3dd5f5
acblackburn <alexcblackburn98@gmail.com>
2020-08-13 14:33:46 +0100

    Create Date and Month_Numeric Columns from other cols in Budget dataset

--- initial_budget_upload.py
+++ initial_budget_upload.py
@@ -11,10 +11,30 @@ with open("metadata.json") as json_file:
 
 df = pd.read_csv("~/Desktop/Budgetv1.csv")
 
-df['Period'] = None
-df['Period_Practice_Weighted_1000'] = None
-df['Period_Practice_Raw_1000'] = None
-df['Period_Divisional_Weighted_1000'] = None
-df['Period_Divisional_Raw_1000'] = None
+# Remove empty rows
+df.dropna(axis=0, how='all', inplace=True)
+
+missing_period_cols = [
+        'Period',
+        'Period_Practice_Weighted_1000',
+        'Period_Practice_Raw_1000',
+        'Period_Divisional_Weighted_1000',
+        'Period_Divisional_Raw_1000'
+        ]
+
+for col in missing_period_cols:
+    df[col] = None
+    df[col] = df[col].astype('Float64')
+
+# Parse 'Date' column from 'Month' and 'Year' columns
+df['Date'] = df['Month'].str.strip() + "/" + df['Year'].astype(str)
+df['Date'] = pd.to_datetime(df['Date'], format="%B/%Y")
+
+# Create a numerical month column from the above created date column
+df['Month_Numeric'] = df['Date'].dt.strftime("%m")
+
+# Make sure Division and CC columns are in uppercase format
+df['Dp'] = df['Dp'].str.upper()
+df['CC'] = df['CC'].str.upper()
 
 print(df.dtypes)

98217ccfa2b1b7fe02558279ddf00517fb9532c6
acblackburn <alexcblackburn98@gmail.com>
2020-08-24 11:53:36 +0100

    Modified table name call in gbq_transfer.py to pick by table name rather than have Budget hard coded

--- localpackage/gbq_transfer.py
+++ localpackage/gbq_transfer.py
@@ -11,7 +11,7 @@ def df_to_gbq(df, dataset, table_name):
     # Open and load json metadata file
     with open("metadata.json") as json_file:
         data = json.load(json_file)
-        metadata = data["Budget"]
+        metadata = data[table_name]
 
     # Create BigQuery schema from json metadata
     schema = [bigquery.SchemaField(entry['bq_name'], entry['bq_dtype']) for entry in metadata if entry['bq_name'] != None]
